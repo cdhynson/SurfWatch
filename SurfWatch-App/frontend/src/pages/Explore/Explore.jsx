f35e9c26721ce66df172a0249effb5b68943a026
Uliyaah Dionisio <86292164+uliyaah@users.noreply.github.com>
2025-05-12 10:32:18 -0700

    Added necessary icons and basic page structure and navigation

--- SurfWatch-App/frontend/src/pages/Explore/Explore.jsx
+++ SurfWatch-App/frontend/src/pages/Explore/Explore.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import BottomNav from "../../components/Navbars/BottomNav";
+import TopNav from "../../components/Navbars/TopNav";
+
+import ForecastSheet from "../../components/Forecast/ForecastSheet"
+
 import "./Explore.css"
 
 function Explore(){
 
   return (<>
+  <TopNav/>
+  <div className="explore-container">
+    <ForecastSheet/>
+  </div>
+  <BottomNav/>
 
-<BottomNav/>
   </>);
 }
 
--- SurfWatch-App/frontend/src/pages/Profile/Profile.jsx
+++ SurfWatch-App/frontend/src/pages/Profile/Profile.jsx
@@ -1,12 +1,49 @@
 import React from "react";
 import BottomNav from "../../components/Navbars/BottomNav";
-import "./Profile.css"
+import TopNav from "../../components/Navbars/TopNav";
+import "./Profile.css";
 
-function Profile(){
-  return (<>
+function Profile() {
+  return (
+    <>
+      <TopNav />
+      <div className="profile-container">
+      <span id="surf-streak">
+            <img src="/assets/fire.svg" alt="Flame Icon" />
+            <p> 3 DAY SURFING STREAK</p>
+            </span>
 
-    <BottomNav/>
-      </>);
+        <div className="pfp-section">
+          <div className="pfp"/>
+          <img src="/assets/pfp.svg" alt="Flame Icon" />
+          <button className="edit-pfp"><img src="/assets/edit.svg" alt="Flame Icon" /></button>
+        </div>
+
+        <h2 className="user-name">Jane Doe</h2>
+      <div className="skill-container">
+        <span className="skill-bar">
+          <span id="skill-r-filled" />
+          <span id="skill-m-filled" />
+          <span id="skill-l" />
+        </span>
+        <p className="skill-label">INTERMEDIATE</p>
+      </div>
+        <button className="add-session-btn">Add Session</button>
+
+        <div className="tab-row">
+          <span className="tab-active">Summary</span>
+          <span className="tab">Sessions</span>
+        </div>
+        <div className="summary">
+
+        </div>
+        <div className="sessions-card">
+          
+        </div>
+      </div>
+      <BottomNav />
+    </>
+  );
 }
 
 export default Profile;

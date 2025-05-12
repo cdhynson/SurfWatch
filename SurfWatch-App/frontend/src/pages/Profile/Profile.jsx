77de81cc8783c89c48400288deb162a0a006fd11
Uliyaah Dionisio <86292164+uliyaah@users.noreply.github.com>
2025-05-12 14:06:07 -0700

    Profile page placeholder styling

--- SurfWatch-App/frontend/src/pages/Profile/Profile.jsx
+++ SurfWatch-App/frontend/src/pages/Profile/Profile.jsx
@@ -3,42 +3,43 @@ import BottomNav from "../../components/Navbars/BottomNav";
 import TopNav from "../../components/Navbars/TopNav";
 import "./Profile.css";
 
+function updateTab(){
+
+}
+
 function Profile() {
   return (
     <>
       <TopNav />
       <div className="profile-container">
-      <span id="surf-streak">
-            <img src="/assets/fire.svg" alt="Flame Icon" />
-            <p> 3 DAY SURFING STREAK</p>
-            </span>
+        <div className="profile-header">
+          <span id="surf-streak">
+                <img src="/assets/fire.svg" alt="Flame Icon" />
+                <p> 3 DAY SURFING STREAK</p>
+          </span>
 
-        <div className="pfp-section">
-          <div className="pfp"/>
-          <img src="/assets/pfp.svg" alt="Flame Icon" />
-          <button className="edit-pfp"><img src="/assets/edit.svg" alt="Flame Icon" /></button>
+          <div className="pfp-section">
+            <div className="pfp"/>
+            <img src="/assets/pfp.svg" alt="Flame Icon" />
+            <button className="edit-pfp"><img src="/assets/edit.svg" alt="Edit" /></button>
+          </div>
+
+          <h2 className="user-name">Jane Doe</h2>
+
+          <div className="skill-container">
+          <img src="/assets/intermediate.svg" alt="Skill Bar" />
+            <p className="skill-label">INTERMEDIATE</p>
+          </div>
+
+          <button className="add-session-btn">Add Session</button>
         </div>
-
-        <h2 className="user-name">Jane Doe</h2>
-      <div className="skill-container">
-        <span className="skill-bar">
-          <span id="skill-r-filled" />
-          <span id="skill-m-filled" />
-          <span id="skill-l" />
-        </span>
-        <p className="skill-label">INTERMEDIATE</p>
-      </div>
-        <button className="add-session-btn">Add Session</button>
-
+        
         <div className="tab-row">
-          <span className="tab-active">Summary</span>
-          <span className="tab">Sessions</span>
+          <span className="summary" onClick={{}}>Summary</span>
+          <span className="sessions">Sessions</span>
         </div>
-        <div className="summary">
+        <div className="tab-content">
 
-        </div>
-        <div className="sessions-card">
-          
         </div>
       </div>
       <BottomNav />

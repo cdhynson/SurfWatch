c7737aebabb3f10c97af5d10351944149f64567a
GurnoorSohal <sohalgurnoor@gmail.com>
2025-06-09 14:35:54 -0700

    done!

--- SurfWatch-App/frontend/src/pages/Home/Home.jsx
+++ SurfWatch-App/frontend/src/pages/Home/Home.jsx
@@ -131,7 +131,7 @@ function Home() {
           <div className="carousel">
             <div className="carousel-item">
               <img
-                src="https://6f90-69-196-44-113.ngrok-free.app/video_feed"
+                src="https://9e0cfe0e91bd.ngrok.app/video_feed"
                 width="640"
                 height="480"
               />
